365733d9e14483f0fe054cc5553d04d78646ceb8
Dmitry Kuklin <dmi-kuklin@yandex.ru>
2023-04-28 00:40:06 +0300

    mobile - kmm: android: QR scanner handler

---
+++ androidApp/src/main/java/com/example/metasecret/android/ImageAnalyzer.kt
@@ -0,0 +1,42 @@
+package com.example.metasecret.android
+
+import android.annotation.SuppressLint
+import androidx.camera.core.ImageAnalysis
+import androidx.camera.core.ImageProxy
+import com.google.mlkit.vision.barcode.Barcode
+import com.google.mlkit.vision.barcode.BarcodeScanning
+import com.google.mlkit.vision.common.InputImage
+
+class ImageAnalyzer : ImageAnalysis.Analyzer {
+
+    override fun analyze(imageProxy: ImageProxy) {
+        scanBarcode(imageProxy)
+    }
+
+    @SuppressLint("UnsafeExperimentalUsageError")
+    private fun scanBarcode(imageProxy: ImageProxy) {
+        imageProxy.image?.let { image ->
+            val inputImage = InputImage.fromMediaImage(image, imageProxy.imageInfo.rotationDegrees)
+            val scanner = BarcodeScanning.getClient()
+            scanner.process(inputImage)
+                .addOnCompleteListener {
+                    imageProxy.close()
+                    if (it.isSuccessful) {
+                        readBarcodeData(it.result as List<Barcode>)
+                    } else {
+                        it.exception?.printStackTrace()
+                    }
+                }
+        }
+    }
+
+    private fun readBarcodeData(barcodes: List<Barcode>) {
+        for (barcode in barcodes) {
+            when (barcode.valueType) {
+                Barcode.TYPE_URL -> {
+                    val url = barcode.url?.url
+                }
+            }
+        }
+    }
+}
--- androidApp/src/main/java/com/example/metasecret/android/WelcomeScannerViewActivity.kt
+++ androidApp/src/main/java/com/example/metasecret/android/WelcomeScannerViewActivity.kt
@@ -1,13 +1,86 @@
 package com.example.metasecret.android
 
+import android.annotation.SuppressLint
 import android.content.Intent
+import android.content.pm.PackageManager
+import android.net.Uri
 import android.os.Bundle
+import android.provider.Settings
+import android.util.Size
 import android.widget.Button
+import android.widget.ImageView
 import androidx.activity.ComponentActivity
+import androidx.camera.core.CameraSelector
+import androidx.camera.core.ImageAnalysis
+import androidx.camera.core.Preview
+import androidx.camera.lifecycle.ProcessCameraProvider
+import androidx.camera.view.PreviewView
+import androidx.core.content.ContextCompat
+import androidx.lifecycle.LifecycleOwner
+import com.google.common.util.concurrent.ListenableFuture
+import java.util.concurrent.ExecutorService
+import java.util.concurrent.Executors
 
 class WelcomeScannerViewActivity: ComponentActivity() {
+    private lateinit var cameraProviderFuture: ListenableFuture<ProcessCameraProvider>
+    private lateinit var cameraExecutor: ExecutorService
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.onboarding_qr_scaner)
+
+        if (checkCameraPermission()) {
+            buildCamera()
+        }
+    }
+
+    private fun checkCameraPermission(): Boolean {
+        if (ContextCompat.checkSelfPermission(
+                this,
+                android.Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED) {
+            Intent().also {
+                it.action = Settings.ACTION_APPLICATION_DETAILS_SETTINGS
+                it.data = Uri.fromParts("package", packageName, null)
+                startActivity(it)
+                finish()
+                return false
+            }
+        }
+        return true
+    }
+
+    private fun buildCamera() {
+        cameraExecutor = Executors.newSingleThreadExecutor()
+        cameraProviderFuture = ProcessCameraProvider.getInstance(this)
+
+        cameraProviderFuture.addListener(Runnable {
+            val cameraProvider = cameraProviderFuture.get()
+            bindPreview(cameraProvider)
+        }, ContextCompat.getMainExecutor(this))
+    }
+
+    @SuppressLint("UnsafeExperimentalUsageError")
+    private fun bindPreview(cameraProvider: ProcessCameraProvider) {
+        val preview: Preview = Preview.Builder()
+            .build()
+        val cameraSelector: CameraSelector = CameraSelector.Builder()
+            .requireLensFacing(CameraSelector.LENS_FACING_BACK)
+            .build()
+
+        val previewView = findViewById<PreviewView>(R.id.previewView)
+        preview.setSurfaceProvider(previewView.createSurfaceProvider(null))
+
+        val analyzer = ImageAnalyzer()
+        val imageAnalysis = ImageAnalysis.Builder()
+            .setTargetResolution(Size(1280, 720))
+            .setBackpressureStrategy(ImageAnalysis.STRATEGY_KEEP_ONLY_LATEST)
+            .build()
+        imageAnalysis.setAnalyzer(cameraExecutor, analyzer)
+
+        cameraProvider.bindToLifecycle(
+            this as LifecycleOwner,
+            cameraSelector,
+            imageAnalysis,
+            preview
+        )
     }
 }
